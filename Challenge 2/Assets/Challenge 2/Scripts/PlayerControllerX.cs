d02de3ec5b3b9b76440d01b5f7997fef7a273e14
david7201 <80122594+david7201@users.noreply.github.com>
2023-10-16 14:29:15 +0100

    challenge 2

---
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float ReduceSpam = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && ReduceSpam <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            ReduceSpam = 1;
+
+        }
+        if(ReduceSpam >= 0){
+            ReduceSpam -= Time.deltaTime;
+
+        }
+        
+    }
+}
